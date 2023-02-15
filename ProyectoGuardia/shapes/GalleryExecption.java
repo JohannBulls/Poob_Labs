4f6f4013361120cd8ff83516653de70f1a5fafad
Sebastian Zamora <Sebassele2008@gmail.com>
2023-02-15 15:54:48 -0500

    Listos Diagramas

---
+++ ProyectoGuardia/shapes/GalleryExecption.java
@@ -0,0 +1,19 @@
+
+/**
+ * Let me create a galery
+ *
+ * @author Sebastian Zamora
+ * @author Johann Amaya
+ * @version 1.0
+ */
+public class GalleryExecption extends Exception
+{
+    
+    /**
+     * Lanza las exepciones creadas.
+     * @param Recibe un mensaje String msm
+    */
+    public GalleryExecption(String msm){
+        super(msm);
+    }
+}
---
+++ ProyectoGuardia/shapes/Guard.java
@@ -0,0 +1,43 @@
+
+/**
+ * Write a description of class Guard here.
+ *
+ * @author Sebastian Zamora
+ * @author Johann Amaya
+ * @version 1.0
+ */
+public class Guard
+{
+    // instance variables - replace the example below with your own
+    private int x,y;
+
+    /**
+     * Constructor for objects of class Guard
+     */
+    public Guard()
+    {
+        
+    }
+
+    /**
+     * 
+     */
+    public void moveGuard(int x,int y)
+    {
+        
+    }
+    
+    /**
+     * Let me make visible the rooms on the Guard
+     */
+    public void makeVisible(){
+        
+    }
+    
+    /**
+     * Let me make visible the rooms on the Guard
+     */
+    public void makeInvisible(){
+        
+    }
+}
