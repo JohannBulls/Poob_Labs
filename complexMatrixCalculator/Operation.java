bf87405245bbf879a99ee7ac90a1d64eb62da8b3
JOHANN AMAYA LOPEZ <40411821+JohannBulls@users.noreply.github.com>
2023-02-25 00:54:39 -0500

    Add files via upload

---
+++ complexMatrixCalculator/Operation.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class Operation here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Operation
+{
+    private int[][][] a;
+  /* public Operation(int[][][] b,  char op,int[][][] c) {
+       String s = String.valueOf(op);
+       if(s=="+"){
+            a=Suma(b,c);
+        }else if("-"){
+            a=Resta(b,c);
+        }else if("*"){
+            a=Multiplicacion(b,c);      
+        }else{
+            JOptionPane.showMessageDialog(null, "Esa opereacion no se puede realizar");
+        }
+ 
+    }
+*/
+    public int[][][] Suma(int[][][] b,int[][][] c){
+        
+        return a;
+    }
+}
